d23981cdcb0a414a1ce377993a640dbfbeddd88a
hungmin.yang <hungmin.yang@gmail.com>
2026-09-19 12:54:18 +0800

    0919 update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='multiplication-planet-v30-all48-recrop';
+const CACHE='multiplication-planet-v31-mobile-token';
 const ASSETS=['./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./dex-full-48.png',...Array.from({length:48},(_,i)=>`./assets/creature-${String(i+1).padStart(2,'0')}.png`)];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
